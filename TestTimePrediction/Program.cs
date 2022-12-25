b9317ad16f62a5a7739ffb612e2bca6e78ad202d
GER\ittayeya <ittay.eyal@intel.com>
2022-12-25 12:44:18 +0200

    Init

---
+++ TestTimePrediction/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TestTimePrediction;
+using Trace.Api.Common.TP;
+using Trace.Api.Common;
+using Trace.Api.Configuration;
+using Trace.Api.Services.TestProgramParser;
+using Trace.Api.Services.TestProgramParser.Interfaces;
+using Trace.Api.Services.Common;
+using Trace.Api.Services.TestResults.ItuffIndex;
+
+Console.WriteLine("Starting...");
+
+var traceParser = new TraceParser();
+var testProgram = traceParser.Parse();
+
+Console.ReadLine();
---
+++ TestTimePrediction/TraceParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trace.Api.Common.TP;
+using Trace.Api.Common;
+using Trace.Api.Configuration;
+using Trace.Api.Services.Common;
+using Trace.Api.Services.TestProgramParser;
+using Trace.Api.Services.TestProgramParser.Interfaces;
+using Trace.Api.Services.TestResults.ItuffIndex;
+using Trace.Brita.Api.Interfaces;
+
+namespace TestTimePrediction
+{
+    internal class TraceParser
+    {
+        public TestProgram Parse()
+        {
+            // object containing IDC network drives map
+            var driveMapping = ConfigurationLoader.GetDriveMapping(SiteEnum.IDC, SiteDataSourceEnum.CLASSHDMT);
+
+            // gets some valid test program path
+            string stplPath, tplPath;
+
+            // get path to stpl and tpl file of latest executed TP
+            GetTestProgramPath(driveMapping, out stplPath, out tplPath);
+
+            // now we need an instance to a test program parser
+            // first we create the factory and ask it for a parser for the relevant test program type            
+            var parserFactory = new TestProgramParserFactory(driveMapping);
+
+            // parse the TP & ask for Plists    
+            TestProgram testProgram = parserFactory.ParseTestProgram(stplPath, tplPath, EnumTpParserFlag.PLists);
+
+            return testProgram;
+        }
+
+        private static void GetTestProgramPath(IDriveMapping driveMapping, out string stplPath, out string tplPath)
+        {
+            // prepare index manager to retrieve jobs info
+            using (var ituffIndexManager = new ItuffIndexManager(driveMapping))
+            {
+                // get latest job info
+                var ituffDefinition = ituffIndexManager
+                    .GetAllItuffDefinitions()
+                    //.Where(i => i.Errors.IsNullOrEmpty())
+                    .MaxBy(i => i.EndDate);
+
+                // depending on EVG version which was used for that job, it may be null
+                stplPath = ituffDefinition.StplPath;
+                tplPath = ituffDefinition.TplPath;
+            }
+        }
+
+    }
+}
